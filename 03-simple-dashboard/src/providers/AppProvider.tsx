40966e93b9713fc8f0df2ec7b9be8984796678b0
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-01 21:02:01 -0500

    feat; install and use QeuryClientProvider

---
+++ 03-simple-dashboard/src/providers/AppProvider.tsx
@@ -0,0 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import {queryClient} from '../lib/react-query'
+
+type Props = {
+    children: React.ReactNode
+}
+
+export const AppProvider = ({children}: Props) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
