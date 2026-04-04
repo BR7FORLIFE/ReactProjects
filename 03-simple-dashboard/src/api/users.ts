aaf47c9dfa98f46f7c7316584b69adafec6737c9
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-04 09:28:13 -0500

    feat: implements getUserByEmail in the Api logic

--- 03-simple-dashboard/src/api/users.ts
+++ 03-simple-dashboard/src/api/users.ts
@@ -1,11 +1,29 @@
 import type { User } from "../types/userType";
 
+const APIURL = "https://jsonplaceholder.typicode.com/users";
+
 export const fetchUsers = async (): Promise<User[]> => {
-    const req = await fetch("https://jsonplaceholder.typicode.com/users");
+    const req = await fetch(APIURL);
 
     if (!req.ok) {
         throw new Error("users not found!");
     }
 
     return (await req.json()) as User[];
+};
+
+export const getUserByEmail = async (email: string) => {
+    try {
+        const req = await fetch(`${APIURL}?email=${email}`);
+
+        if (!req.ok) {
+            return { data: null, error: "user not found!" };
+        }
+
+        const data = await req.json();
+
+        return { data, error: null };
+    } catch (error) {
+        return { data: null, error: "Network error" };
+    }
 };
