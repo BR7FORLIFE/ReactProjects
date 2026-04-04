e19b57538f28a2334c30914550ee841c4a479190
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-04 09:27:31 -0500

    feat: use the new UserForm

--- 03-simple-dashboard/src/App.tsx
+++ 03-simple-dashboard/src/App.tsx
@@ -1,9 +1,11 @@
 import ListUser from "./components/UserList"
 import { ReactQueryDevtools } from "react-query-devtools"
+import UserForm from "./components/CreateUserForm"
 
 function App() {
   return (
     <>
+      <UserForm/>
       <ListUser/>
       <ReactQueryDevtools/>
     </>
